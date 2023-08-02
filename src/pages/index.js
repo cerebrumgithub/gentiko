dbe8444de958ce25ed1a0940b6833676cba36b39
Mark Diamat <mm8mjed@gmail.com>
2023-08-02 11:50:15 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,11 +12,11 @@ export default function Home() {
         <Script src="https://www.googletagmanager.com/gtag/js?id=G-F13KLVV0CG" />
         <Script id="google-analytics">
           {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-            gtag('config', 'G-F13KLVV0CG');
+  gtag('config', 'G-F13KLVV0CG');
           `}
         </Script>
       </div>
