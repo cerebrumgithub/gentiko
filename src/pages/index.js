91db46841f1696969acc4d36496f1d5f87b16c3f
Mark Diamat <mm8mjed@gmail.com>
2023-08-08 14:46:16 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,24 +27,24 @@ export default function Home() {
       <div id="fb-customer-chat" className="fb-customerchat"></div>
       <Script id="chat-plugin" strategy="lazyOnload">
         {`
-            var chatbox = document.getElementById('fb-customer-chat');
-            chatbox.setAttribute("page_id", "YOUR_PAGE_ID");
-            chatbox.setAttribute("attribution", "biz_inbox");
+      var chatbox = document.getElementById('fb-customer-chat');
+      chatbox.setAttribute("page_id", "118912747813161");
+      chatbox.setAttribute("attribution", "biz_inbox");
       
-            window.fbAsyncInit = function() {
-              FB.init({
-                xfbml            : true,
-                version          : 'v17.0'
-              });
-            };
+      window.fbAsyncInit = function() {
+        FB.init({
+          xfbml            : true,
+          version          : 'v17.0'
+        });
+      };
       
-            (function(d, s, id) {
-              var js, fjs = d.getElementsByTagName(s)[0];
-              if (d.getElementById(id)) return;
-              js = d.createElement(s); js.id = id;
-              js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-              fjs.parentNode.insertBefore(js, fjs);
-            }(document, 'script', 'facebook-jssdk'));
+      (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
         `}
       </Script>
     </div>
