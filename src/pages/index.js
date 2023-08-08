fd0a82e5f23a9a9fe6255e8248fa6c51cf9ce7e5
Mark Diamat <mm8mjed@gmail.com>
2023-08-08 13:42:47 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,6 +20,36 @@ export default function Home() {
           `}
         </Script>
       </div>
+
+    <div id="fb-root"></div>
+    <div id="fb-customer-chat" class="fb-customerchat">
+    </div>
+
+    <Script>{`
+      var chatbox = document.getElementById('fb-customer-chat');
+      chatbox.setAttribute("page_id", "118912747813161");
+      chatbox.setAttribute("attribution", "biz_inbox");    
+    `}
+    </Script>
+
+    <Script>{`
+      window.fbAsyncInit = function() {
+        FB.init({
+          xfbml            : true,
+          version          : 'v17.0'
+        });
+      };
+      
+      (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));      
+    `}
+    </Script>
+  
       <Carousel/>    
         <div className={`${home.dividerParent} flex justify-center`}>
             <div className={`${home.divider}`} style={{width: '20%', backgroundColor: '#B64248'}}></div>
